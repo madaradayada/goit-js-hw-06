b1174a0ff3b436c94f54ca7e3cbcda807d5ead40
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-06-23 14:49:01 +0300

    start

---
+++ js/test.js
@@ -0,0 +1,13 @@
+"use strict";
+const x = 5;
+const y = 10;
+const z = 5;
+
+console.log("x > y:", x > y); // false
+console.log("x < y:", x < y); // true
+console.log("x < z:", x < z); // false
+console.log("x <= z:", x <= z); // true
+console.log("x === y:", x === y); // false
+console.log("x === z:", x === z); // true
+console.log("x !== y:", x !== y); // true
+console.log("x !== z:", x !== z); // false
