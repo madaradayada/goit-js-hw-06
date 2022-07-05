5206017d2bab0371c7485742180fc53e76e9baaa
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-07-05 14:17:16 +0300

    1

--- js/test.js
+++ js/test.js
@@ -1,13 +1,28 @@
-"use strict";
-const x = 5;
-const y = 10;
-const z = 5;
+//mod 3.14 function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
 
-console.log("x > y:", x > y); // false
-console.log("x < y:", x < y); // true
-console.log("x < z:", x < z); // false
-console.log("x <= z:", x <= z); // true
-console.log("x === y:", x === y); // false
-console.log("x === z:", x === z); // true
-console.log("x !== y:", x !== y); // true
-console.log("x !== z:", x !== z); // false
+// const keys = Object.keys(object);
+
+//   for (const key of keys) {
+
+//       propCount += 1;
+    
+//   }
+  
+
+//   return propCount;
+//   // Change code above this line
+// }
+// console.log(countProps({}))
+// console.log(countProps({ name: "Mango", age: 2 }))
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }))
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+// Change code below this line
+const keys = Object.keys(apartment);
+const values = Object.values(apartment);
