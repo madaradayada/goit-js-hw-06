9c8d986b36fdb1d80c08bd655abe04c2df14f166
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-07-25 15:06:26 +0300

    test1

--- js/test.js
+++ js/test.js
@@ -18,25 +18,25 @@
 // console.log(countProps({ name: "Mango", age: 2 }))
 // console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }))
 
-const pizzaPalace = {
-  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
-  order(pizzaName, onSuccess, onError) {
-    if (this.pizzas.includes(pizzaName)) {
-      return onSuccess(pizzaName);
-    }
-    return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
-  }
-}; 
+// const pizzaPalace = {
+//   pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return onSuccess(pizzaName);
+//     }
+//     return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+//   }
+// }; 
 
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
-}
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
 
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
 
-console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));   
-console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
-console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
-console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));   
+// console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
