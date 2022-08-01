00ce6a30bcc5af0cde9bb34af5f71df7bc45279d
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-08-01 15:46:06 +0300

    fix bug 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,16 +2,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const changeColorButton = document.querySelector('.change-color');
-const colorValue = document.querySelector('color');
-const bodyEl = document.querySelector('body');
+const changeColorButton = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
 
-colorValue.textContent = '#ffffff';
 
 changeColorButton.addEventListener('click', onCangeColorButton);
 
 function onCangeColorButton(event) {
   bodyEl.style.backgroundColor = getRandomHexColor();
-  colorValue.innerHTML = `${getRandomHexColor()}`;
+  colorValue.textContent = getRandomHexColor();
   console.log(getRandomHexColor());
 }
