4de7c17b8a16754f4e6b3a4e6826c711a3a34f43
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-08-04 17:37:39 +0300

    доработка 2-3 задания

--- js/task-03.js
+++ js/task-03.js
@@ -13,4 +13,19 @@ const images = [
   },
 ];
 
-for(let el of images) document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', `<li><img src="${el.url}" alt="${el.alt}"></li>`);
+const list = document.querySelector('.gallery')
+
+const markup = images
+	.map(image => `<li><img class="picture" src=${image.url} width = '400' alt='${image.alt}'></img></li>`)
+	.join('')
+
+  list.insertAdjacentHTML('beforeend', markup)
+    // const img = document.createElement('img')
+  
+const pictures = document.querySelectorAll('.picture')
+
+list.style.display = 'flex';
+list.style.flexWrap = 'column-reverse'
+list.style.listStyle = 'none'
+list.style.gap = '30px'
+list.style.justifyContent = 'center'
